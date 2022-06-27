9632320cbdd40c379c6b735edde8381da599bf8b
GioKas93 <giorgio_castellucci@libero.it>
2022-06-27 17:53:43 +0000

    Stag bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -13,9 +13,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/voltage_raphael.mk
+    $(LOCAL_DIR)/stag_raphael.mk
 
 COMMON_LUNCH_CHOICES := \
-    voltage_raphael-user \
-    voltage_raphael-userdebug \
-    voltage_raphael-eng
+    stag_raphael-user \
+    stag_raphael-userdebug \
+    stag_raphael-eng
